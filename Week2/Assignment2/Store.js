ddb7f51342f0531e03df7bce684235e74fddec8d
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-03-29 21:09:54 -0400

    Week 2 assignment 2

---
+++ Week2/Assignment2/Store.js
@@ -0,0 +1,8 @@
+const todos =[
+		{text: "Finish Projects at work"},
+		{text: "Complete homnework assignments"},
+		{text: "Fold laundry"},
+		{text: "Do the dishes"},
+		{text: "Complete volunteer work"}
+	];
+export default todos
