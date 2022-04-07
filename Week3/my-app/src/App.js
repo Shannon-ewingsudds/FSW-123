8dc85f83741adaf3d42c0192b3b778fd4130ada8
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-04-06 22:43:54 -0400

    week 3 roll dice

---
+++ Week3/my-app/src/App.js
@@ -0,0 +1,13 @@
+import RollDice from './RollDice.js';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+    <h1>Let's Play Shannon's Dice Game !</h1> 
+    <br></br>
+    <RollDice />
+  </>
+   
+  );
+}
