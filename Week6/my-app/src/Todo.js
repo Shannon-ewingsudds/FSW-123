adef793c3bb8f8d5552b0c3e0f77975bfcdd6f1e
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-04-28 18:09:37 -0400

    update week 6

--- Week6/my-app/src/Todo.js
+++ Week6/my-app/src/Todo.js
@@ -1,29 +1,100 @@
+import React from "react";
 import { useState } from 'react';
-import EditTodoForm from './TodoForm';
+import TodoForm from "./TodoForm";
 
-function Todo({todo, completeTodo, deleteTodo, editTodo}) {
-    const [isChecked, setIsChecked] = useState(todo.isCompleted);
-    const [isEdited, setIsEdited] = useState(false);
+function Todo({ todo, comp, del, edit }) {
+
+    const [editToggle, setEditToggle] = useState(false);
+    const [input, setInput] = useState('');
+
+    const getInput = (input) => {
+        setInput(input.currentTarget.value);
+    };
+
+    const onSubmitEdit = (e) => {
+        if (input == ''){
+            alert('Todo can not be empty!')
+        } else {
+            e.preventDefault();
+            edit(input, todo.id);
+            setInput('');
+            setEditToggle((prevToggle) => !prevToggle);
+        };
+    };
+
+    const onCancel = (e) => {
+        e.preventDefault();
+        setInput('');
+        setEditToggle((prevToggle) => !prevToggle);
+    }
+
+    const onEditClick = () => {
+        setEditToggle((prevToggle) => !prevToggle);
+    }
+
+    const buttonStyle = {
+        backgroundColor: 'rgb(230, 90, 90)',
+        borderRadius: '50%',
+        marginLeft: '10px'
+    };
     
-    const handleChange = (text) => {
-        todo.isCompleted = !isChecked;
-        !isChecked ? document.getElementById(text).disabled = true : document.getElementById(text).disabled = false;
-        setIsChecked(!isChecked);
-    }
-
-    const handleEdit = () => {
-        setIsEdited(!isEdited);
-    }
-
     return (
-        isEdited ? <EditTodoForm todo ={todo} id ={`editForm ${todo.id}`} cancelBtn ={handleEdit} editTodo ={editTodo}/> : 
-        <li key ={todo.id} id={todo.id}>
-            <input type="checkbox" checked ={isChecked} onChange ={() => handleChange(`editBtnFor ${todo.text}`)}/>
-            <span style ={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>{todo.text}</span>
-            <button id ={`deleteBtnFor ${todo.text}`} onClick ={() => deleteTodo(todo.id)}>X</button>
-            <button id ={`editBtnFor ${todo.text}`} onClick ={handleEdit}>Edit</button>
-        </li>
+
+        <div style = {{textDecoration: todo.isCompleted ? 'line-through' : ''}}>
+            { !editToggle ? 
+                <>
+
+                    <input 
+                    onChange = {() => comp(todo.id)} 
+                    value = {todo.isCompleted} 
+                    checked = {todo.isCompleted} 
+                    type = 'checkbox'
+                    required>
+                    </input>
+                    {todo.text}
+                    <button 
+                        onClick = {() => del(todo.id)}
+                        className = 'del-button'
+                        style = {buttonStyle}>
+                        X
+                    </button>
+                    <button
+                        className = 'edit-button'
+                        onClick = {onEditClick}
+                        disabled={todo.isCompleted}>
+                        Edit
+                    </button>
+
+                </>
+
+                : 
+
+                <>
+                    <form>
+                        <input 
+                            className = 'edit-field' 
+                            type = 'text'
+                            onChange = {getInput}
+                            required>
+                        </input>
+                        <button
+                            onClick = {onSubmitEdit}
+                            className = 'update-button'
+                            style = {{backgroundColor:"rgba(4, 197, 4, 0.699)"}}>
+                            Update
+                        </button>
+                        <button 
+                            onClick = {onCancel}
+                            className = 'cancel-button'
+                            style = {{backgroundColor:"rgba(4, 197, 4, 0.699)"}}>
+                            Cancel
+                        </button>
+                    </form>
+                </>
+            }
+        </div>
+
     );
-}
+};
 
 export default Todo;
